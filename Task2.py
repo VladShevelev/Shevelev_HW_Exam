fa48ef26386a9ee0cca12863528ab91128e4a1d3
VladShevelev <121121981+VladShevelev@users.noreply.github.com>
2023-01-16 01:18:39 +0300

    Add files via upload
    Task 1 - https://www.codewars.com/kata/56dec885c54a926dcd001095/javascript
    Task 2 - https://www.codewars.com/kata/5715eaedb436cf5606000381

---
+++ Task2.py
@@ -0,0 +1,15 @@
+#You get an array of numbers, return the sum of all of the positives ones.
+
+#Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+#Note: if there is nothing to sum, the sum is default to 0.
+
+
+def positive_sum(arr):
+    sum = 0
+
+    for i in range(len(arr)):
+        if arr[i] > 0:
+            sum += arr[i]
+        
+    return(sum)
